cbeec3ba6f9d0bd690bb04130c169cb2edc3cab9
MP <mp@mps-macbook-pro.lan>
2020-01-18 03:01:09 -0500

    wip

--- public/platinum.js
+++ public/platinum.js
@@ -132,7 +132,6 @@ export class PlatinumElement extends HTMLElement {
         return _[key]
       },
       set: (_, key, value) => {
-        console.log('setProxy', key, value)
         _[key] = value
         this[`$${key}`] = value
         this.$inject(key, value)
